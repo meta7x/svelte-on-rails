1c6048d41eb2d05f5fa1678a00dfab7b50bdfd32
Dimitri <dimitri@MacBook-Pro.local>
2020-05-29 14:44:27 +0200

    ignore generated files

--- ssr/components.js
+++
@@ -1,9 +0,0 @@
-import renderSvelte from './hypernova-svelte.js';
-
-const name = 'App'
-let App;
-import(`../app/javascript/components/${name}.svelte`).then(({ default: component }) => {
-    App = renderSvelte(component);
-})
-
-export { App };
